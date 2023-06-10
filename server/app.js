12566f69ba83e499bbc52f0c54edc165e98304cf
DeividRogerMR <deividroger.1995@gmail.com>
2023-06-10 17:17:07 -0300

    atuaizado appjs

--- server/app.js
+++ server/app.js
@@ -22,6 +22,7 @@ app.use(passport.session());
 app.use(
   cors({
     origin: `${process.env.CLIENT_URL}`,
+    sameSite: "None",
     methods: "GET,POST,PUT,DELETE, OPTIONS",
     credentials: true,
   })
